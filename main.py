5cbae326491266dd155f9b73f0c4f86c719a634e
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-11 18:01:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import base64
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 st.write("## Remove background from your image")
-st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
-)
 st.write("## Upload and download :gear:")
 my_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
